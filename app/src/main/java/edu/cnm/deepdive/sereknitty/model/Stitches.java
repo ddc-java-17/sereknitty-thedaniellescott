1ad266d880d16ed89c0a89db07c3589df89fa33b
DanielleScott <dmyers28@cnm.edu>
2024-02-15 12:02:59 -0700

    Added a couple more stitches to Stitches, and added alphanumeric shorthand for included stitches.

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/Stitches.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/Stitches.java
@@ -2,13 +2,16 @@ package edu.cnm.deepdive.sereknitty.model;
 
 public enum Stitches {
 
-  KNIT,
-  PURL,
-  KNIT_2_TOGETHER, // K2T
-  PURL_2_TOGETHER, // P2T
-  YARN_OVER, // YO
-  MAKE_1_RIGHT, // M1R
-  MAKE_1_LEFT; // M1L
+  // TODO: 2/15/2024 Find images for the knitting symbols.
+  KNIT("K"),
+  PURL("P"),
+  KNIT_2_TOGETHER("K2tog"), // K2T
+  PURL_2_TOGETHER("P2tog"), // P2T
+  YARN_OVER("YO"), // YO
+  MAKE_1_RIGHT("M1R"), // M1R
+  MAKE_1_LEFT("M1L"),
+  KNIT_3_TOGETHER("K3tog"),
+  ; // M1L
 
 // TODO: 2/12/2024 Figure out how to notate cabling.
 
