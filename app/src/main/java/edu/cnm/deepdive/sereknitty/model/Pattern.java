518615495bf010d0b15959fde4b0cd39c2fc1b44
DanielleScott <dmyers28@cnm.edu>
2024-02-27 14:22:54 -0700

    Fixed Pattern

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/Pattern.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/Pattern.java
@@ -4,22 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pattern extends Project {
-/*
+
   private final String name;
   private final List<Row> rows;
-
 // file type?
 
-
-  Pattern() {
-    Pattern pattern = new pattern();
-    pattern = new List<Row>();
-
-    // Pattern is a list of rows.
+  public Pattern(String id, Pattern pattern, int currentRow, int currentPositionInRow, String name,
+      List<Row> rows) {
+    super(id, pattern, currentRow, currentPositionInRow);
+    this.name = name;
+    this.rows = rows;
   }
+
   // TODO: 2/12/2024  Figure out how to display a picture the user submits or takes.
-
-
-*/
 
 }
