f8d4f29fa566f9b92754c9145b74af88bb8f9efc
DanielleScott <dmyers28@cnm.edu>
2024-02-27 14:46:59 -0700

    removed comment code from project

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/Row.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/Row.java
@@ -7,25 +7,8 @@ public class Row {
 
   private final List<Stitch> stitch;
 
-  public Row(List<Stitch> stitch) {
+  Row(List<Stitch> stitch) {
     this.stitch = stitch;
   }
 
-  /*
-  public Row() {
-    Row row = new Row();
-    row = new List<Stitch>();
-
-    // Row = List <Stitches>, but in a way that the compiler understands.
-    //Maybe Stream.toArray of user input? (plus a few other steps obviously!)
-  }
-
-  public addStitch(Stitch stitch) {
-    this.stitch.add(stitch);
-  }
-
-  public List<Stitch> getStitch() {
-    return stitch;
-  }
-*/
 }
