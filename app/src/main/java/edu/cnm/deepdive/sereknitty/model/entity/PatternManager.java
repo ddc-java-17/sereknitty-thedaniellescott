9b330c831634136d773e16e2e68b2b36b8a202c5
DanielleScott <dmyers28@cnm.edu>
2024-03-03 12:40:11 -0700

    Updated UML and attempting to fix PatternManager.

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/entity/PatternManager.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/entity/PatternManager.java
@@ -3,24 +3,15 @@ package edu.cnm.deepdive.sereknitty.model.entity;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-@Entity(
-    tableName = "pattern_manager",
-    indices =  ,
-    foreignKeys = @ForeignKey(
-        entity = User.class,
-        childColumns = "user_id",
-        parentColumns = "user_id",
-        onDelete = ForeignKey.CASCADE
-        ),
-    @ForeignKey(
-        entity = Pattern.class,
-        childColumns = "pattern_id",
-        parentColumns =
-    )
-
-)
+@Entity
+@Table(name = "pattern_manager", indexes = {
+    @Index(columnList = "pattern_manager_id, user_id"),
+})
+@JsonInclude(Include.NON_NULL)
+@JsonPropertyOrder({"pattern_manager_id"})
 public class PatternManager {
 
   @PrimaryKey(autoGenerate = true)
