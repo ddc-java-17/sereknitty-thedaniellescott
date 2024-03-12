8755b995d21962930a2a12766f2a1fa104e03747
DanielleScott <dmyers28@cnm.edu>
2024-03-11 18:48:11 -0600

    Adding fragments; navigation in progress.

---
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/controller/PatternLibraryFragment.java
@@ -0,0 +1,62 @@
+package edu.cnm.deepdive.sereknitty.controller;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.cnm.deepdive.sereknitty.R;
+
+/**
+ * A simple {@link Fragment} subclass. Use the {@link PatternLibraryFragment#newInstance} factory
+ * method to create an instance of this fragment.
+ */
+public class PatternLibraryFragment extends Fragment {
+
+  // TODO: Rename parameter arguments, choose names that match
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "param1";
+  private static final String ARG_PARAM2 = "param2";
+
+  // TODO: Rename and change types of parameters
+  private String mParam1;
+  private String mParam2;
+
+  public PatternLibraryFragment() {
+    // Required empty public constructor
+  }
+
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided
+   * parameters.
+   *
+   * @param param1 Parameter 1.
+   * @param param2 Parameter 2.
+   * @return A new instance of fragment PatternLibraryFragment.
+   */
+  // TODO: Rename and change types and number of parameters
+  public static PatternLibraryFragment newInstance(String param1, String param2) {
+    PatternLibraryFragment fragment = new PatternLibraryFragment();
+    Bundle args = new Bundle();
+    args.putString(ARG_PARAM1, param1);
+    args.putString(ARG_PARAM2, param2);
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      mParam1 = getArguments().getString(ARG_PARAM1);
+      mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_pattern_library, container, false);
+  }
+}
