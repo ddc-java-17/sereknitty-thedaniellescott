8cc4e87c20b59bd477aa55ef685baa5f2804a15f
DanielleScott <dmyers28@cnm.edu>
2024-03-19 14:42:48 -0600

    PatternRepository still in progress: added get() and constructor.

--- app/src/main/java/edu/cnm/deepdive/sereknitty/service/PatternRepository.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/service/PatternRepository.java
@@ -1,8 +1,12 @@
 package edu.cnm.deepdive.sereknitty.service;
 
 
+import androidx.lifecycle.LiveData;
 import edu.cnm.deepdive.sereknitty.model.dao.PatternDao;
 import edu.cnm.deepdive.sereknitty.model.entity.Pattern;
+import edu.cnm.deepdive.sereknitty.model.entity.User;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -17,9 +21,7 @@ import javax.inject.Singleton;
 @Singleton
 public class PatternRepository {
 
-
   private final PatternDao patternDao;
-
 
 
   /**
@@ -30,4 +32,17 @@ public class PatternRepository {
   PatternRepository(PatternDao patternDao) {
     this.patternDao = patternDao;
   }
+
+  /**
+   * Returns a {@link LiveData}-based query for the {@link Pattern} entity instance identified by
+   * {@code id}. The query executes when observed, or (if already being observed) whenever the
+   * contents of the underlying {@code user} table are modified using Room data-access methods.
+   *
+   * @param id Unique identifier (primary key value) of a {@link Pattern} entity instance.
+   * @return {@link LiveData}-based query for the {@link Pattern} identified by {@code id}.
+   */
+  public LiveData<Pattern> get(long id) {
+    return patternDao.select(id);
+  }
+
 }
