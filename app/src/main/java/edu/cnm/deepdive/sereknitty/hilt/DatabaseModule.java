b720237323a98168bea42b0df53bb513b58d18d1
DanielleScott <dmyers28@cnm.edu>
2024-03-20 05:25:09 -0600

    Finished Hilt Module. Running into build problem I solved in other commits. Need to merge main into navigation

--- app/src/main/java/edu/cnm/deepdive/sereknitty/hilt/DatabaseModule.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/hilt/DatabaseModule.java
@@ -8,6 +8,8 @@ import dagger.hilt.InstallIn;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 import edu.cnm.deepdive.sereknitty.model.dao.PatternDao;
+import edu.cnm.deepdive.sereknitty.model.dao.RowDao;
+import edu.cnm.deepdive.sereknitty.model.dao.RowStitchDao;
 import edu.cnm.deepdive.sereknitty.model.dao.UserDao;
 import edu.cnm.deepdive.sereknitty.service.SereknittyDatabase;
 import javax.inject.Singleton;
@@ -24,25 +26,67 @@ public final class DatabaseModule {
     // Package-private constructor to avoid automatic HTML documentation generation.
   }
 
+  /**
+   * Provides a single instance of {@link SereknittyDatabase}.
+   *
+   * @param context
+   * @return {@link SereknittyDatabase}
+   */
   @Provides
   @Singleton
-  SereknittyDatabase provideLocalDatabase(@ApplicationContext Context context) {
+  public SereknittyDatabase provideLocalDatabase(@ApplicationContext Context context) {
     return Room
         .databaseBuilder(context, SereknittyDatabase.class, SereknittyDatabase.NAME)
         .addCallback(new SereknittyDatabase.Callback())
         .build();
   }
 
+  /**
+   * Provides a single instance of {@link UserDao}.
+   *
+   * @param database
+   * @return {@link UserDao}
+   */
   @Provides
-  UserDao provideUserDao(SereknittyDatabase database) {
+  @Singleton
+  public UserDao provideUserDao(SereknittyDatabase database) {
     return database.getUserDao();
   }
 
+  /**
+   * Provides a single instance of {@link PatternDao}.
+   *
+   * @param database
+   * @return {@link PatternDao}
+   */
   @Provides
-  PatternDao providePatternDao(SereknittyDatabase database) {
+  @Singleton
+  public PatternDao providePatternDao(SereknittyDatabase database) {
     return database.getPatternDao();
   }
 
-  // TODO Add additional methods so satisfy dependencies on other DAO interface implementations.
+  /**
+   * Provides a single instance of {@link RowDao}.
+   *
+   * @param database
+   * @return {@link RowDao}
+   */
+  @Provides
+  @Singleton
+  public RowDao provideRowDao(SereknittyDatabase database) {
+    return database.getRowDao();
+  }
+
+  /**
+   * Provides a single instance of {@link RowStitchDao}.
+   *
+   * @param database
+   * @return {@link RowStitchDao}
+   */
+  @Provides
+  @Singleton
+  public RowStitchDao provideRowStitchDao(SereknittyDatabase database) {
+    return database.getRowStitchDao();
+  }
 
 }
