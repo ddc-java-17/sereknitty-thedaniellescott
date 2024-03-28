82f5d5e990f277cdd481abb441fff59f6af3e811
DanielleScott <dmyers28@cnm.edu>
2024-03-28 17:46:17 -0600

    Implemented references to the drawable resources in Stitch. Javadocs for model classes in progress.

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/Row.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/Row.java
@@ -3,10 +3,19 @@ package edu.cnm.deepdive.sereknitty.model;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class represents a row in knitting. In knitting, a row is made up of stitches, and a
+ * knitted work is made up of many rows. In this program, a {@link Row} is made up of any given
+ * number of {@code stitch} from the {@link Stitch} {@link Enum}.
+ */
 public class Row {
 
   private final List<Stitch> stitch;
 
+  /**
+   *
+   * @param stitch
+   */
   Row(List<Stitch> stitch) {
     this.stitch = stitch;
   }
--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/Stitch.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/Stitch.java
@@ -2,32 +2,54 @@ package edu.cnm.deepdive.sereknitty.model;
 
 import edu.cnm.deepdive.sereknitty.R;
 
+/**
+ * This is an {@link Enum} that holds all the stitch values my app will use. Think of it like a
+ * stitch dictionary.
+ */
 public enum Stitch {
 
-
-// TODO: 3/24/2024 Apply my images for the knitting symbols to the correct stitch.
-
-  KNIT("K"),
-  PURL("P"),
-  KNIT_2_TOGETHER("K2tog"),
-  SLIP_SLIP_KNIT("SSK"),
-  PURL_2_TOGETHER("P2tog"),
-  SLIP_SLIP_PURL("SSP"),
-  YARN_OVER("YO"),
-  MAKE_1_RIGHT("M1R"),
-  MAKE_1_LEFT("M1L"),
-  KNIT_3_TOGETHER("K3tog"),
-  PURL_3_TOGETHER("P3tog"),
-  CENTER_DOUBLE_DECREASE("CDD");
+  KNIT("K", 0),
+  PURL("P", R.drawable.purl),
+  KNIT_2_TOGETHER("K2tog", R.drawable.knit_2_together),
+  SLIP_SLIP_KNIT("SSK", R.drawable.slip_slip_knit),
+  PURL_2_TOGETHER("P2tog", R.drawable.purl_2_together),
+  SLIP_SLIP_PURL("SSP", R.drawable.slip_slip_purl),
+  YARN_OVER("YO", R.drawable.yarn_over),
+  MAKE_1_RIGHT("M1R", R.drawable.make_1_right),
+  MAKE_1_LEFT("M1L", R.drawable.make_1_left),
+  KNIT_3_TOGETHER("K3tog", R.drawable.knit_3_together),
+  PURL_3_TOGETHER("P3tog", R.drawable.purl_3_together),
+  CENTER_DOUBLE_DECREASE("CDD", R.drawable.center_double_decrease);
 
   private final String shorthandAlphaNum;
+  private final int image;
 
-  Stitch(String shorthandAlphaNum) {
+  /**
+   * This constructor initializes all the fields and their assets.
+   *
+   * @param shorthandAlphaNum {@link String} shorthand for the stitch.
+   * @param drawableId {@link int} representing the image for the symbolic shorthand for the stitch.
+   */
+  Stitch(String shorthandAlphaNum, int drawableId) {
     this.shorthandAlphaNum = shorthandAlphaNum;
+    this.image = drawableId;
   }
 
+  /**
+   * This getter returns the alphanumeric shorthand for the stitches.
+   *
+   * @return shorthandAlphaNum
+   */
   public String shorthandAlphaNum() {
     return shorthandAlphaNum;
   }
 
+  /**
+   * This getter returns the drawable image that will show the symbolic shorthand for the stitches.
+   *
+   * @return image
+   */
+  public int getImage() {
+    return image;
+  }
 }
