d5db7d43c3636492059325ebd6e8375172417b57
DanielleScott <dmyers28@cnm.edu>
2024-04-01 09:15:52 -0600

    JavaDocs for POJOs done.

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/PatternWithRows.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/PatternWithRows.java
@@ -4,15 +4,26 @@ import androidx.room.Relation;
 import edu.cnm.deepdive.sereknitty.model.entity.Pattern;
 import java.util.List;
 
+/**
+ * This POJO is a class that allows for a multi-table join allowing me to recall the {@code rows}
+ * associated with a particular {@link Pattern} instance. It is not an entity, but it allows for
+ * more complicated queries without complicated SQL code.
+ */
 public class PatternWithRows extends Pattern {
 
   @Relation(entityColumn = "pattern_id", parentColumn = "pattern_id")
   private List<RowWithStitches> rows;
 
+  /**
+   * This getter gets the {@link List} of {@code rows} associated with a particular {@link Pattern}.
+   */
   public List<RowWithStitches> getRows() {
     return rows;
   }
 
+  /**
+   * This setter sets the {@link List} of {@code rows} associated with a particular {@link Pattern}.
+   */
   public void setRows(List<RowWithStitches> rows) {
     this.rows = rows;
   }
--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/RowWithStitches.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/RowWithStitches.java
@@ -5,16 +5,26 @@ import edu.cnm.deepdive.sereknitty.model.entity.Row;
 import edu.cnm.deepdive.sereknitty.model.entity.RowStitch;
 import java.util.List;
 
-
+/**
+ * This POJO is a class that allows for a multi-table join allowing me to recall the
+ * {@code stitches} associated with a particular {@link Row} instance. It is not entity, but it
+ * allows for more complicated queries without complicated SQL code.
+ */
 public class RowWithStitches extends Row {
 
   @Relation(entityColumn = "row_id", parentColumn = "row_id")
   private List<RowStitch> stitches;
 
+  /**
+   * This getter gets the {@link List} of stitches associated with a particular {@link Row}.
+   */
   public List<RowStitch> getStitches() {
     return stitches;
   }
 
+  /**
+   * This setter sets the {@link List} of stitches associates with a particular {@link Row}.
+   */
   public void setStitches(List<RowStitch> stitches) {
     this.stitches = stitches;
   }
