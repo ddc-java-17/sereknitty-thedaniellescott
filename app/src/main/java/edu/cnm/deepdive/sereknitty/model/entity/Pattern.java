c1c19b069de28b12d6d7fc6763c98cf8acb16965
DanielleScott <dmyers28@cnm.edu>
2024-04-01 09:52:49 -0600

    Finished JavaDocs for Pattern entity class.

--- app/src/main/java/edu/cnm/deepdive/sereknitty/model/entity/Pattern.java
+++ app/src/main/java/edu/cnm/deepdive/sereknitty/model/entity/Pattern.java
@@ -10,6 +10,9 @@ import androidx.room.PrimaryKey;
 import java.time.Instant;
 import java.util.UUID;
 
+/**
+ * This entity class represents a knitting pattern and holds its data structures.
+ */
 @Entity(
     tableName = "pattern",
     indices = {
@@ -59,82 +62,139 @@ public class Pattern {
   @ColumnInfo(name = "current_row_id", index = true)
   private Long currentRowId;
 
-
+  /**
+   * This getter gets the {@code id} for a pattern.
+   */
   public long getId() {
     return id;
   }
 
+  /**
+   * This setter sets the {@code id} for a pattern.
+   */
   public void setId(long id) {
     this.id = id;
   }
 
+  /**
+   * This getter gets the {@link UUID} key for a pattern.
+   */
   public UUID getKey() {
     return key;
   }
 
+  /**
+   * This setter sets the {@link UUID} key for a pattern.
+   */
   public void setKey(UUID key) {
     this.key = key;
   }
 
+  /**
+   * This getter gets the timestamp for the {@link Instant} the pattern was created.
+   */
   @NonNull
   public Instant getCreated() {
     return created;
   }
 
+  /**
+   * This setter sets the timestamp for the {@link Instant} the pattern was created.
+   */
   public void setCreated(@NonNull Instant created) {
     this.created = created;
   }
 
+  /**
+   * This getter gets the timestamp for the {@link Instant} the pattern was modified.
+   */
   @NonNull
   public Instant getModified() {
     return modified;
   }
 
+  /**
+   * This setter sets the timestamp for the {@link Instant} the pattern was modified.
+   */
   public void setModified(@NonNull Instant modified) {
     this.modified = modified;
   }
 
+  /**
+   * This getter gets the name of a pattern.
+   */
   public String getPatternName() {
     return patternName;
   }
 
+  /**
+   * This setter gets the {@link String} name of a pattern.
+   */
   public void setPatternName(String patternName) {
     this.patternName = patternName;
   }
 
+  /**
+   * This getter gets the description associated with a pattern.
+   */
   public String getPatternDescription() {
     return patternDescription;
   }
 
+  /**
+   * This setter sets the description associated with a pattern.
+   */
   public void setPatternDescription(String patternDescription) {
     this.patternDescription = patternDescription;
   }
 
+  /**
+   * This getter gets the image associated with a pattern.
+   */
   public String getImage() {
     return image;
   }
 
+  /**
+   * This setter sets the image associated with a pattern.
+   */
   public void setImage(String image) {
     this.image = image;
   }
 
+  /**
+   * This getter gets the {@code userId} that created the pattern.
+   */
   public Long getUserId() {
     return userId;
   }
 
+  /**
+   * This setter sets the {@code userId} that created the pattern.
+   */
   public void setUserId(Long userId) {
     this.userId = userId;
   }
 
+  /**
+   * This getter gets the current row id that makes up part of the current stitch location for this
+   * pattern.
+   */
   public Long getCurrentRowId() {
     return currentRowId;
   }
 
+  /**
+   * This setter sets the current row id that makes up part of the current stitch location for this
+   * pattern.
+   */
   public void setCurrentRowId(Long currentRowId) {
     this.currentRowId = currentRowId;
   }
 
-
+  /**
+   * This method generates a {@link UUID} {@code key} for any pattern instance.
+   */
   private void generateKey() {
     key = UUID.randomUUID();
   }
